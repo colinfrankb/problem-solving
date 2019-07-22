d5d1e1d97c549d97ea0c63cc4a9c22c0fd530d28
Colin Bob <cbob@ecentric.co.za>
2019-07-22 18:01:14 +0200

    WeightForWeight

---
+++ WeightForWeight/WeightForWeight.Tests/WeightSortTests.cs
@@ -0,0 +1,25 @@
+using System;
+using NUnit.Framework;
+using WeightForWeight.Framework;
+
+namespace WeightForWeight.Tests
+{
+    [TestFixture]
+    public class WeightSortTests
+    {
+
+        //[Test]
+        //public void Test1()
+        //{
+        //    Console.WriteLine("****** Basic Tests");
+        //    Assert.AreEqual("2000 103 123 4444 99", WeightSort.orderWeight("103 123 4444 99 2000"));
+        //    Assert.AreEqual("11 11 2000 10003 22 123 1234000 44444444 9999", WeightSort.orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
+        //}
+
+        [Test]
+        public void Returns_Parsed_Input()
+        {
+            Assert.AreEqual("1 2 3 4", WeightSort.orderWeight(" 1   2  3 4   "));
+        }
+    }
+}
