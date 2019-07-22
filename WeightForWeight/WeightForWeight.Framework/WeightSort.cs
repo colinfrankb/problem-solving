2cf8e504021202eda657c87da2a8864b48dcc6f1
Colin <colinfrankb@gmail.com>
2019-07-22 20:06:58 +0200

    WeightForWeight project structure

---
+++ WeightForWeight/WeightForWeight.Framework/WeightSort.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace WeightForWeight.Framework
+{
+    public class WeightSort
+    {
+        public static string orderWeight(string strng)
+        {
+            string[] weights = Regex.Split(strng, @"\s+")
+                .Where(x => !string.IsNullOrEmpty(x))
+                .ToArray();
+
+            return string.Join(" ", weights);
+        }
+    }
+}
