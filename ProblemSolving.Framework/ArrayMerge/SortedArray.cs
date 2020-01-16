46c4470a656c283a8f61fb5e5113a6aa57cbe44c
Colin Bob <cbob@ecentric.co.za>
2020-01-16 11:14:29 +0200

    Problem setup

--- ProblemSolving.Framework/ArrayMerge/SortedArray.cs
+++ ProblemSolving.Framework/ArrayMerge/SortedArray.cs
@@ -8,53 +8,9 @@ namespace ProblemSolving.Framework.ArrayMerge
     {
         public class Solution
         {
-            //Axioms
-            //A and B can be variable lengths
             public int[] solution(int[] A, int[] B)
             {
-                //1, 3, 5, 7, 9
-                //2, 4, 6, 8, 10
-
-                //1, 2, 3, 4, 
-                //5, 6
-                int[] result = new int[A.Length + B.Length];
-
-                for (int i = 0, j = 0, k = 0; i < result.Length;)
-                {
-                    if (j < A.Length && k < B.Length)
-                    {
-                        if (A[j] < B[k])
-                        {
-                            result[i] = A[j];
-                            j++;
-                        }
-                        else
-                        {
-                            result[i] = B[k];
-                            k++;
-                        }
-
-                        i++;
-                    }
-                    else
-                    {
-                        if (j < A.Length)
-                        {
-                            result[i] = A[j];
-                            i++;
-                            j++;
-                        }
-
-                        if (k < B.Length)
-                        {
-                            result[i] = B[k];
-                            i++;
-                            k++;
-                        }
-                    }
-                }
-
-                return result;
+                
             }
         }
     }
--- ProblemSolving.Framework/ArrayMerge/UnsortedArray.cs
+++ ProblemSolving.Framework/ArrayMerge/UnsortedArray.cs
@@ -6,27 +6,7 @@
         {
             public int[] solution(int[] A, int[] B)
             {
-                // 1, 1, 1, 1
-                // 2, 2
-
-                int[] result = new int[A.Length + B.Length];
-
-                for (int i = 0, j = 0; i < result.Length; j++)
-                {
-                    if (j < A.Length)
-                    {
-                        result[i] = A[j];
-                        i++;
-                    }
-
-                    if (j < B.Length)
-                    {
-                        result[i] = B[j];
-                        i++;
-                    }
-                }
-
-                return result;
+                
             }
         }
     }
